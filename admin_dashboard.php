06ebd081a9ab0e82c335d59e6e184680101a1eb2
kevineaid <kevindevdebug@gmail.com>
2019-07-17 14:04:07 +0700

    title dan nav dinamis

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,3 +1,7 @@
+<?php 
+    $title = "Dashboard | Admin";
+    $page = "admin_dashboard";
+?>
 <?php include 'header.php'; ?>
     
     <div class="page-wrapper">
--- admin_karyawan.php
+++ admin_karyawan.php
@@ -1,3 +1,7 @@
+<?php 
+    $title = "Karyawan | Admin";
+    $page = "admin_karyawan";
+?>
 <?php include 'header.php'; ?>
     
     <div class="page-wrapper">
